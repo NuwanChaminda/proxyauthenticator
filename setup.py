01e1a4e2d6ddb2f09a3146fd4daa3d3e9fa7568e
Nuwan Chaminda <mr.nuwanchaminda@gmail.com>
2021-12-27 13:18:43 +0530

    change version to 003

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="proxyauthenticator",
-    version="0.0.2",
+    version="0.0.3",
     author="Nuwan Chaminda",
     author_email="lokupodda123@gmail.com",
-    description="Proxy Authenticator for python projects",
+    description="Proxy Authenticator for python projects",s
     long_description="file: README.md",
     long_description_content_type="text/markdown",
     url="https://github.com/NuwanChaminda/proxyauthenticator",
